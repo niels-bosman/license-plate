d1f272afba35e37eb6375cced3339e1ecdb63c9c
Niels Bosman <nielsbosman@mail.com>
2021-07-25 13:50:07 +0200

    Changed: Cleanup of code. Changed: tsconfig.json. Added: Basic README.md.

--- src/LicensePlate.ts
+++ src/LicensePlate.ts
@@ -1,6 +1,6 @@
 export default class LicensePlate {
 	/**
-	 * All Dutch available sidecodes.
+	 * All Dutch sidecodes.
 	 *
 	 * @private
 	 * @type {RegExp[]}
@@ -12,14 +12,14 @@ export default class LicensePlate {
 		/^[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}$/, // [4]  => XX-99-XX
 		/^[a-zA-Z]{2}[a-zA-Z]{2}[0-9]{2}$/, // [5]  => XX-XX-99
 		/^[0-9]{2}[a-zA-Z]{2}[a-zA-Z]{2}$/, // [6]  => 99-XX-XX
-		/^[0-9]{2}[a-zA-Z]{3}[0-9]{1}$/,    // [7]  => 99-XXX-9
-		/^[0-9]{1}[a-zA-Z]{3}[0-9]{2}$/,    // [8]  => 9-XXX-99
-		/^[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{1}$/, // [9]  => XX-999-X
-		/^[a-zA-Z]{1}[0-9]{3}[a-zA-Z]{2}$/, // [10] => X-999-XX
-		/^[a-zA-Z]{3}[0-9]{2}[a-zA-Z]{1}$/, // [11] => XXX-99-X
-		/^[a-zA-Z]{1}[0-9]{2}[a-zA-Z]{3}$/, // [12] => X-99-XXX
-		/^[0-9]{1}[a-zA-Z]{2}[0-9]{3}$/,    // [13] => 9-XX-999
-		/^[0-9]{3}[a-zA-Z]{2}[0-9]{1}$/,    // [14] => 999-XX-9
+		/^[0-9]{2}[a-zA-Z]{3}[0-9]$/,       // [7]  => 99-XXX-9
+		/^[0-9][a-zA-Z]{3}[0-9]{2}$/,       // [8]  => 9-XXX-99
+		/^[a-zA-Z]{2}[0-9]{3}[a-zA-Z]$/,    // [9]  => XX-999-X
+		/^[a-zA-Z][0-9]{3}[a-zA-Z]{2}$/,    // [10] => X-999-XX
+		/^[a-zA-Z]{3}[0-9]{2}[a-zA-Z]$/,    // [11] => XXX-99-X
+		/^[a-zA-Z][0-9]{2}[a-zA-Z]{3}$/,    // [12] => X-99-XXX
+		/^[0-9][a-zA-Z]{2}[0-9]{3}$/,       // [13] => 9-XX-999
+		/^[0-9]{3}[a-zA-Z]{2}[0-9]$/,       // [14] => 999-XX-9
 	];
 
 	/**
@@ -36,7 +36,7 @@ export default class LicensePlate {
 	 * @private
 	 * @type {number}
 	 */
-	private static matchedSidecode: number;
+	private static sidecode: number;
 
 	/**
 	 * Builds a new license plate based on input.
@@ -46,8 +46,8 @@ export default class LicensePlate {
 	 * @returns {string} The formatted license plate.
 	 */
 	public static make(licensePlate: string): string {
-		this.licensePlate    = this.getPrettyLicensePlate(licensePlate);
-		this.matchedSidecode = this.getSidecode();
+		this.licensePlate = this.getPrettyLicensePlate(licensePlate);
+		this.sidecode     = this.getSidecode();
 
 		return this.format();
 	}
@@ -59,9 +59,9 @@ export default class LicensePlate {
 	 * @return {number | undefined} The index of the sidecode.
 	 */
 	private static getSidecode(): number {
-		for (let index = 0; index < this.sidecodes.length; index++) {
-			if (this.licensePlate.match(this.sidecodes[index])) {
-				return index + 1;
+		for (let sidecode of this.sidecodes) {
+			if (this.licensePlate.match(sidecode)) {
+				return this.sidecodes.indexOf(sidecode) + 1;
 			}
 		}
 
@@ -86,23 +86,23 @@ export default class LicensePlate {
 	 * @returns {string} The formatted license plate.
 	 */
 	private static format(): string {
-		if (this.matchedSidecode <= 6) {
+		if (this.sidecode <= 6) {
 			return `${this.licensePlate.substr(0, 2)}-${this.licensePlate.substr(2, 2)}-${this.licensePlate.substr(4, 2)}`;
 		}
 
-		if ([7, 9].includes(this.matchedSidecode)) {
+		if ([7, 9].includes(this.sidecode)) {
 			return `${this.licensePlate.substr(0, 2)}-${this.licensePlate.substr(2, 3)}-${this.licensePlate.substr(5, 1)}`;
 		}
 
-		if ([8, 10].includes(this.matchedSidecode)) {
+		if ([8, 10].includes(this.sidecode)) {
 			return `${this.licensePlate.substr(0, 1)}-${this.licensePlate.substr(1, 3)}-${this.licensePlate.substr(4, 2)}`;
 		}
 
-		if ([11, 14].includes(this.matchedSidecode)) {
+		if ([11, 14].includes(this.sidecode)) {
 			return `${this.licensePlate.substr(0, 3)}-${this.licensePlate.substr(3, 2)}-${this.licensePlate.substr(5, 1)}`;
 		}
 
-		if ([12, 13].includes(this.matchedSidecode)) {
+		if ([12, 13].includes(this.sidecode)) {
 			return `${this.licensePlate.substr(0, 1)}-${this.licensePlate.substr(1, 2)}-${this.licensePlate.substr(3, 3)}`;
 		}
 
